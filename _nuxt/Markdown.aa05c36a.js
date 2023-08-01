001ad6531b5582443c4f88a00747335f3ee81a79
YoungMayor <youngmayor.dev@gmail.com>
2023-08-01 18:43:55 +0000

    Deploying to gh-pages from @ YoungMayor/content-wind-2@2f8419f1befe57b0ab6df5cfdad46f3555e02c3d 🚀

---
+++ _nuxt/Markdown.aa05c36a.js
@@ -0,0 +1,1 @@
+import r from"./ContentSlot.e9b6e8f9.js";import{d as s,a4 as u,I as f,al as l}from"./entry.456e00f5.js";const m=s({name:"Markdown",extends:r,setup(t){const{parent:e}=l(),{between:n,default:a}=u(),o=f(()=>typeof t.unwrap=="string"?t.unwrap.split(" "):["*"]);return{fallbackSlot:a,tags:o,between:n,parent:e}}});export{m as default};
